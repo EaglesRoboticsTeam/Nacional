e5fc162ac32f43239fd30cdb2526a4019b6ca0e6
EaglesRoboticsTeam <38738763+EaglesRoboticsTeam@users.noreply.github.com>
2018-10-12 11:57:28 -0300

    Update sleipnir.py

--- sleipnir.py
+++ sleipnir.py
@@ -1,11 +1,8 @@
-#Copyright (c) 2018 Eagles Robotics Team
-#Código base https://github.com/EaglesRoboticsTeam/BrickPi3/blob/master/Software/Python/Examples/EV3-Color_Sensor_Color.py
 import brickpi3
-from time import sleep
+from time import sleep, strftime
 
 BP = brickpi3.BrickPi3()
 
-#Definindo os sensores de cor EV3
 BP.set_sensor_type(BP.PORT_1, BP.SENSOR_TYPE.EV3_COLOR_COLOR)
 BP.set_sensor_type(BP.PORT_2, BP.SENSOR_TYPE.EV3_COLOR_COLOR)
 BP.set_sensor_type(BP.PORT_3, BP.SENSOR_TYPE.EV3_COLOR_COLOR)
@@ -13,16 +10,75 @@ BP.set_sensor_type(BP.PORT_4, BP.SENSOR_TYPE.EV3_COLOR_COLOR)
 
 color = ["none", "Black", "Blue", "Green", "Yellow", "Red", "White", "Brown"]
 
-#Função dos motores
 def move(pw1, pw2):
-    #18-19 Reseta os motores
-    BP.offset_motor_encoder(BP.PORT_C, BP.get_motor_encoder(port1))
-    BP.offset_motor_encoder(BP.PORT_B, BP.get_motor_encoder(port2))
-    #Inicia o movimento dos motores
+    BP.offset_motor_encoder(BP.PORT_C, BP.get_motor_encoder(BP.PORT_C))
+    BP.offset_motor_encoder(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B))
     BP.set_motor_power(BP.PORT_C, pw1)
     BP.set_motor_power(BP.PORT_B, pw2)
 
-#Loop do seguidor
+def status():
+    print("[{}] S1: {} | S2: {} | S3: {} | S4: {} ".format(strftime('%H:%M:%S'),color[c1],color[c2],color[c3],color[c4]))
+
+def v_left():
+    while color[c3] == "Green":
+        move(40,40)
+    if color[c3] == "Black":
+        move(-20,-20)
+        sleep(0.4)
+        if color[c2] == "Green":
+            move(100,-100)
+            sleep(2)
+        else:
+            move(40,40)
+            sleep(0.4)
+            while color[c2] == "White":
+                move(75,-55)
+            move(-55,75)
+            sleep(0.2)
+            move(-50,-50)
+            sleep(0.3)
+
+def v_right():
+    while color[c2] == "Green":
+        move(40,40)
+    if color[c2] == "Black":
+        move(-20,-20)
+        sleep(0.4)
+        if color[c3] == "Green":
+            move(100,-100)
+            sleep(2)
+        else:
+            move(40,40)
+            sleep(0.4)
+            while color[c3] == "White":
+                move(-55,75)
+            move(75,-55)
+            sleep(0.2)
+            move(-50,-50)
+            sleep(0.3)
+
+def ponta_esc():
+    if color[c3] == "Black":
+        move(40,40)
+        sleep(0.3)
+        while color[c2] == "White":
+            move(75,-55)
+        move(-55,75)
+        sleep(0.2)
+        move(-50,-50)
+        sleep(0.3)
+
+def ponta_dir():
+    if color[c2] == "Black":
+        move(40,40)
+        sleep(0.3)
+        while color[c3] == "White":
+            move(-55,75)
+        move(75,-55)
+        sleep(0.2)
+        move(-50,-50)
+        sleep(0.3)
+
 try:
     while True:
         try:
@@ -31,43 +87,30 @@ try:
             c3 = BP.get_sensor(BP.PORT_3)
             c4 = BP.get_sensor(BP.PORT_4)
 
-            if color[c4] == "Black":
-                if color[c1] == "Black":
-                    move(-20,-20)
-                    sleep(0.3)
-                    if color[c3] == "Green":
-                        if color[c2] == "Green":
-                            move(100,-100)
-                            sleep(0.8)
-                        else:
-                            move(10,10)
-                            sleep(0.2)
-                            move(-75,100)
-                            sleep(0.4)
-                    elif color[c2] == "Green":
-                        if color[c3] == "Green":
-                            move(-100,100)
-                            sleep(0.8)
-                        else:
-                            move(10,10)
-                            sleep(0.2)
-                            move(100,-75)
-                            sleep(0.4)
-                elif color[c1] == "White":
-                    move(-75, 100)
-                    sleep(0.4)
+            if color[c3] == "Green":
+                v_left()
+            elif color[c2] == "Green":
+                v_right()
+            elif color[c4] == "Black":
+                ponta_esc()
+            elif color[c1] == "Black":
+                ponta_dir()
+                
             elif color[c4] == "White":
                 if color[c1] == "White":
                     if color[c3] == "Black":
                         if color[c2] == "White":
-                            move(-75,100)    
+                            move(75,-55)
+                            status()
                     elif color[c2] == "Black":
                         if color[c3] == "White":
-                            move(100,-75)
+                            move(-55,75)
+                            status()
                     elif color[c2] == "White":
-                        move(50,50)
-                        #go
+                        move(40,40)
+                        status()
+
         except brickpi3.SensorError as error:
             print(error)
 except KeyboardInterrupt:
-    BP.reset_all()
+	BP.reset_all()
